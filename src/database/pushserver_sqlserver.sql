749a8dba500918676664cb6b2b8a4ae8f1c8c9f1
Hamza Öztürk <hamza.ozturk@busoft.com.tr>
2022-05-25 10:36:05 +0300

    Missing database scripts added.

--- src/database/pushserver_sqlserver.sql
+++ src/database/pushserver_sqlserver.sql
@@ -5,7 +5,7 @@ CREATE TABLE ofPushServer (
     node        VARCHAR(1024)           NULL,
     secret      VARCHAR(1024)           NULL,
     type        VARCHAR(16)             NOT NULL,
-    PRIMARY KEY (domain, deviceId)
+    CONSTRAINT ofPushServer_pk PRIMARY KEY (domain, deviceId)
 );
 
 INSERT INTO ofVersion (name, version) VALUES ('pushserver', 1);
